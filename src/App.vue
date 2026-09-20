c693ac3c76c2efd06c47e444390280bd2c06bc05
Victor Kovalchuk <vict02r05@gmail.com>
2026-09-20 11:15:47 +0300

    creating a basic vue app

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+    <h1>My Friends</h1>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        { 
+          id: 'manuel', 
+          name: 'Manuel Lorenz', 
+          phone: '01234 5678 991', 
+          email: 'manuel@localhost.com' 
+        },
+        { 
+          id: 'julies', 
+          name: 'Julie Jones', 
+          phone: '09876 543 221', 
+          email: 'julie@localhost.com' 
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
